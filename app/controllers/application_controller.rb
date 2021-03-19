e9b990b70d3ffb3c4b1679c52013ea7472b31a74
Allison _ <allimacq@gmail.com>
2021-03-19 18:24:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,28 @@
+require "./config/environment"
+require "./app/models/recipe"
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+  end
+  
+  get '/recipes/:id' do
+  end
 
+  get '/recipes/:id/edit' do
+  end
+
+  get '/recipes/new' do
+  end
+  
+  patch '/recipes/:id' do
+  end
+  
+  delete '/recipes/:id' do
+  end
+  
 end
